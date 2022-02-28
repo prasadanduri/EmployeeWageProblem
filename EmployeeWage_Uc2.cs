fd2f92a7e708e6456a55937e35170906a9097912
prasad <prasadanduri13gmail.com>
2022-02-28 17:05:46 +0530

    Employeewage problem

---
+++ EmployeeWage_Uc2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeWage_Uc2
+    {
+        public static void CalculateEmployeeWage()
+        {
+            int IS_FULL_TIME = 1;
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Emp Wage:" + empWage);
+
+
+        }
+    }
+}
