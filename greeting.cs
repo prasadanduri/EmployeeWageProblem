699c0404d4fa27d2b08e14326118d5507b677c5f
prasad <prasadanduri13gmail.com>
2022-02-28 13:04:42 +0530

    greeting employeewahe

---
+++ greeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class greeting
+    {
+        public static void Employee()
+        {
+            Console.WriteLine("Welcome to Employee Wage");
+            Console.ReadLine(); 
+        }
+        
+    }
+}
